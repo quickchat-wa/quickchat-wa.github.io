28f7e07205a934a4a05df69257b1cb6871f0ea6c
naveenccmsd <naveenccmsd@users.noreply.github.com>
2025-04-11 18:17:36 +0000

    deploy: quickchat-wa/quickchat-wa@f8ab610f5b6ea96a2dcd0fd26e356fcf6c6a7b9e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "version.json": "f0ec1b72868276d13de4c71e938e1ddd",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"main.dart.js": "da16132e920c8ca7585e9a189ae45d5d",
+"main.dart.js": "95bbfbde00973907c1d744a4efa96e8f",
 "icons/Icon-maskable-512.png": "b8a67c6568e97324069d1da50db923bc",
 "icons/Icon-192.png": "b8bf28e6439f915df5b3661302981710",
 "icons/Icon-512.png": "b8a67c6568e97324069d1da50db923bc",
 "icons/Icon-maskable-192.png": "b8bf28e6439f915df5b3661302981710",
 "manifest.json": "43cbf117025ddd4286640789200d87a3",
 "favicon.png": "97ae20e7f9b135153aa82fcd796e8a86",
-"flutter_bootstrap.js": "3d7e5f5f767bb5d973f4c635ce76caca"};
+"flutter_bootstrap.js": "97650ae1805eaa465784666e8749dc2d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
